25f9bee9dbe9318365bc0a4ac56ee49acdd82218
rob587 <robertocamm02@yahoo.com>
2025-07-31 15:54:48 +0200

    array creato

--- index.js
+++ index.js
@@ -6,9 +6,48 @@ const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) =>{
-    res.send('Server del mio Blog ')
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+    res.send('server del mio blog')
 })
+
+app.get('/list', (req, res) =>{
+    const list =  [
+        {
+            title: "giacomo",
+            contenuto: "Lorem ipsum",
+            immagine: "img/fanta4.png",
+            tags: "Lorem ipsum"
+        },
+        {
+            title: "giacomo",
+            contenuto: "Lorem ipsum",
+            immagine: "img/fanta4.png",
+            tags: "Lorem ipsum"
+        },
+        {
+            title: "giacomo",
+            contenuto: "Lorem ipsum",
+            immagine: "img/fanta4.png",
+            tags: "Lorem ipsum"
+        },
+        {
+            title: "giacomo",
+            contenuto: "Lorem ipsum",
+            immagine: "img/fanta4.png",
+            tags: "Lorem ipsum"
+        },
+        {
+            title: "giacomo",
+            contenuto: "Lorem ipsum",
+            immagine: "img/fanta4.png",
+            tags: "Lorem ipsum"
+        },
+        
+    ]
+})
+
 
 app.listen(port, ()=>{
     console.log('Server del mio blog')
